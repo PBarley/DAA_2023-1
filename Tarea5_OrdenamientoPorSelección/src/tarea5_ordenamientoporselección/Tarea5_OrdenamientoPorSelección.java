ec9bf08a889cc3d2448972ded9a5ad11b46822ea
PBarley <72161914+PBarley@users.noreply.github.com>
2022-09-22 20:50:55 -0500

    Tarea 5 con análisis

--- Tarea5_OrdenamientoPorSelección/src/tarea5_ordenamientoporselección/Tarea5_OrdenamientoPorSelección.java
+++ Tarea5_OrdenamientoPorSelección/src/tarea5_ordenamientoporselección/Tarea5_OrdenamientoPorSelección.java
@@ -9,7 +9,7 @@ public class Tarea5_OrdenamientoPorSelección {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        int[] dato = {23,12,8,7,1,4,3,2,11};
+        int[] dato = {23,12,8,7,1,4,3,2,11}; 
         
         System.out.println("Arreglo:");
         for (int i = 0; i < dato.length; i++)
@@ -25,26 +25,27 @@ public class Tarea5_OrdenamientoPorSelección {
         }
     }
     
-    public static void seleccion (int[] arr)
+    public static void seleccion (int[] arr) //S(n)= 1       
     {
-        int pivote = 0;
-        int temp = 0;
-        int aux = 0;
-        int min = 0;
+        int pivote = 0; //T(n)= 1   S(n)= 1
+        int temp = 0;   //T(n)= 1   S(n)= 1
+        int aux = 0;    //T(n)= 1   S(n)= 1
+        int min = 0;    //T(n)= 1   S(n)= 1
         
-        for(pivote = 0; pivote < arr.length; pivote++)
+        for(pivote = 0; pivote < arr.length; pivote++) //T(n)= 4n + 2n^2   S(n)= 0
         {
-            min = pivote;
-            for(aux = pivote+1;aux < arr.length; aux++)
+            min = pivote; //T(n)= 1   S(n)= 0
+            for(aux = pivote+1;aux < arr.length; aux++) //T(n)= 2n   S(n)= 0
             {
-                if(arr[aux] < arr[min]){
-                    min = aux;
+                if(arr[aux] < arr[min]){ //T(n)= 1   S(n)= 0
+                    min = aux;           //T(n)= 1   S(n)= 0
                 }
             }
             
-            temp = arr[pivote];
-            arr[pivote] = arr[min];
-            arr[min] = temp;
+            temp = arr[pivote]; //T(n)= 1   S(n)= 0
+            arr[pivote] = arr[min]; //T(n)= 1   S(n)= 0
+            arr[min] = temp; //T(n)= 1   S(n)= 0
         }
     }
+    // T(n) -> O(n)= n^2         S(n) -> O(n)= 5 = O(c)
 }
